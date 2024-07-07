866b794d8578ae18a46e7392b4acd38e5066c05b
Nojaas <jason.leroy@hetic.net>
2024-07-07 22:38:03 +0200

    fix dashboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -179,7 +179,9 @@ export default function Example() {
             className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
           />
         </div>
-        <Dashboard />
+        <div className="mx-auto max-w-5xl pb-32">
+          <Dashboard />
+        </div>
       </div>
     </div>
   );
